2ee830fd4ac29508293503ca55d103c533a183ff
Sergio <sergio.almendros.diaz@gmail.com>
2014-01-27 12:46:44 +0100

    Working in the python script

--- Task 4 - Connecting with opencities/Python/main.py
+++ Task 4 - Connecting with opencities/Python/main.py
@@ -1,7 +1,12 @@
 #Main script
 import sys
 import geojson
+import datetime
+import httplib
+import urllib
 
+ID = 0
+temperature = int(sys.argv[1])
 #1- Collect the data.
 
 if len(sys.argv) != 2:
@@ -14,22 +19,88 @@ if len(sys.argv) != 2:
 print "Reading the logData to check if the new values are new"
 logData = open('logData', 'r')
 
-for line in logData:
-    lastLine = line
+if not logData.read(1):
+    ID = 0
+else:
+    for line in logData:
+        lastLine = line
 
-if int(sys.argv[1]) == int(lastLine):
-    print "The temperature has not change since last update"
+    p = lastLine.split("-")
+    ID = int(p[0]) + 1
+
+#print ID
+if ID != 0:
+    if temperature == int(p[1]):
+        print "The temperature has not change since last update"
+    else:
+        logData.close()
+        logData = open('logData', 'a')
+        logData.write('\n'+str(ID) + '-' + str(temperature))
+        ID += 1
+        print "The new value of temperature has been added to the logData file"
+
 else:
     logData.close()
     logData = open('logData', 'a')
-    logData.write('\n' + sys.argv[1])
+    logData.write('-'+str(ID) + '-' + str(temperature))
+    ID += 1
     print "The new value of temperature has been added to the logData file"
 
 #4- Create the GeoJSON.
+timestamp = datetime.datetime.now().isoformat()
 
-p = geojson.FeatureCollection()
+p = geojson.FeatureCollection(
+    name='Temperature Invented value1',
+    timestamp=timestamp,
+    features=[
+        geojson.Feature(
+            type='Feature',
+            tags=['red', 'tall', 'cheap'],
+            geometry=geojson.Point([2.167028, 41.387547]),
+            properties={
+                'id': "%s" % ID,
+                'name': "%s" % 'SENSOR-TEMP-BCN-UPFPOBLENOU',
+                'datasetId': "%s" % 'temperature',
+                'datasetName': "%s" % 'temperature',
+                'address': "%s" % 'Carrer de Tanger, Barcelona',
+                'description': "%s" % 'Temperature sensor of UPF Poblenou',
+                'timeStamp': "%s" % timestamp,
+                'value': "%s" % temperature,
+                'unit': "%s" % '*C'
+                #'contentType': "%s" % 'video/ogg',
+                #'url': "%s" % 'http://download.blender.org/peach/trailer/trailer_1080p.ogg',
+                #'previewImage': "%s" % 'http://peach.blender.org/wp-content/uploads/poster_rodents_small.jpg'
+            }
+        )
+
+    ]
+)
 data = geojson.dumps(p)
 print data
 
-
 #5- Do the POST to opencities.
+print "Sending the data to opencities"
+
+
+params = urllib.urlencode(data)
+headers = {"Content-type": "application/json"}
+conn = httplib.HTTPConnection("http://opencities.upf.edu")
+conn.request("POST", "/osn2/api/datasets/uploadGeoJson/7b1611c3-c688-474b-bcab-6e4921bfb109/temperature", params, headers)
+response = conn.getresponse()
+print response.status, response.reason
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
