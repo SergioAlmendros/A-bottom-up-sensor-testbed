4a01f0696a5419db590c643421f2fd9b3b66d243
Sergio <sergio.almendros.diaz@gmail.com>
2014-02-11 17:27:20 +0100

    Starting the android development

---
+++ Workspace-Android/A-bottom-up-sensor-testbed/gen/com/example/a_bottom_up_sensor_testbed/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.a_bottom_up_sensor_testbed;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
