a20a01a8d8f73d92f3746a6e6492c645a6315038
Sergio <sergio.almendros.diaz@gmail.com>
2014-06-11 12:19:22 +0200

    changes

--- Collect Data From Sensor Nodes/collect.m
+++ Collect Data From Sensor Nodes/collect.m
@@ -1,61 +1,69 @@
 function collect()
 
-  fid = fopen ("collect1.in", 'r');
+  fid = fopen ("logData1", 'r');
   i = 0;
   while ( feof(fid) == 0 )
     i = i+1;
     p = fgetl(fid);
     #disp(p)
     r = regexp(p, ' ', 'split');
+    
+    if(i < 62)
+      temp1(i) = str2double(r(2));
+      hum1(i) = str2double(r(3));
+      noise1(i) = str2double(r(4));
+      light1(i) = str2double(r(5));
+      gas1(i) = str2double(r(6));
+      time1(i) = r(7);
+    end
 
-    temp1(i) = str2double(r(1));
-    hum1(i) = str2double(r(2));
-    noise1(i) = str2double(r(3));
-    light1(i) = str2double(r(4));
-    gas1(i) = str2double(r(5));
-    time1(i) = r(6);
   end
   fclose(fid);
   
-  fid = fopen ("collect2.in", 'r');
+  fid = fopen ("logData2", 'r');
   i = 0;
   while ( feof(fid) == 0 )
     i = i+1;
     p = fgetl(fid);
     #disp(p)
     r = regexp(p, ' ', 'split');
+    if(i < 62)
+      temp2(i) = str2double(r(2));
+      hum2(i) = str2double(r(3));
+      noise2(i) = str2double(r(4));
+      light2(i) = str2double(r(5));
+      gas2(i) = str2double(r(6));
+      time2(i) = r(7);
+    end
 
-    temp2(i) = str2double(r(1));
-    hum2(i) = str2double(r(2));
-    noise2(i) = str2double(r(3));
-    light2(i) = str2double(r(4));
-    gas2(i) = str2double(r(5));
-    time2(i) = r(6);
   end
   fclose(fid);
   
-  fid = fopen ("collect3.in", 'r');
+  fid = fopen ("logData3", 'r');
   i = 0;
   while ( feof(fid) == 0 )
     i = i+1;
     p = fgetl(fid);
     #disp(p)
     r = regexp(p, ' ', 'split');
-
-    temp3(i) = str2double(r(1));
-    hum3(i) = str2double(r(2));
-    noise3(i) = str2double(r(3));
-    light3(i) = str2double(r(4));
-    gas3(i) = str2double(r(5));
-    time3(i) = r(6);
+    if(i < 62)
+      temp3(i) = str2double(r(2));
+      hum3(i) = str2double(r(3));
+      noise3(i) = str2double(r(4));
+      light3(i) = str2double(r(5));
+      gas3(i) = str2double(r(6));
+      time3(i) = r(7);
+    end
+    
   end
   fclose(fid);
 
-  figure
-  plot(1:i+1,temp1,'r');
+
+  figure 1
+  plot(1:61,temp1,'r');
   hold on
-  plot(1:i+1,temp2,'b');
-  plot(1:i+1,temp3,'g');
+  plot(1:61,temp2,'b');
+  plot(1:61,temp3,'g');
   hold off
   xlabel( 'Temperature' );
   ylabel( 'Time' );
@@ -63,52 +71,52 @@ function collect()
   legend('Arduino 1', 'Arduino 2', 'Arduino 3')
   saveas (1, "../Final\ Report/Figures/GraphicTemperature.png");
   
-  figure
-  plot(1:i+1,hum1,'r');
+  figure 2
+  plot(1:61,hum1,'r');
   hold on
-  plot(1:i+1,hum2,'b');
-  plot(1:i+1,hum3,'g');
+  plot(1:61,hum2,'b');
+  plot(1:61,hum3,'g');
   hold off
   xlabel( 'Humidity' );
   ylabel( 'Time' );
   title( "Humidity" );
   legend('Arduino 1', 'Arduino 2', 'Arduino 3')
-  saveas (1, "../Final\ Report/Figures/GraphicHumidity.png");
+  saveas (2, "../Final\ Report/Figures/GraphicHumidity.png");
   
-  figure
-  plot(1:i+1,noise1,'r');
+  figure 3
+  plot(1:61,noise1,'r');
   hold on
-  plot(1:i+1,noise2,'b');
-  plot(1:i+1,noise3,'g');
+  plot(1:61,noise2,'b');
+  plot(1:61,noise3,'g');
   hold off
   xlabel( 'Noise' );
   ylabel( 'Time' );
   title( "Noise" );
   legend('Arduino 1', 'Arduino 2', 'Arduino 3')
-  saveas (1, "../Final\ Report/Figures/GraphicNoise.png");
+  saveas (3, "../Final\ Report/Figures/GraphicNoise.png");
   
-  figure
-  plot(1:i+1,light1,'r');
+  figure 4
+  plot(1:61,light1,'r');
   hold on
-  plot(1:i+1,light2,'b');
-  plot(1:i+1,light3,'g');
+  plot(1:61,light2,'b');
+  plot(1:61,light3,'g');
   hold off
   xlabel( 'Light' );
   ylabel( 'Time' );
   title( "Light" );
   legend('Arduino 1', 'Arduino 2', 'Arduino 3')
-  saveas (1, "../Final\ Report/Figures/GraphicLight.png");
+  saveas (4, "../Final\ Report/Figures/GraphicLight.png");
   
-  figure
-  plot(1:i+1,gas1,'r');
+  figure 5
+  plot(1:61,gas1,'r');
   hold on
-  plot(1:i+1,gas2,'b');
-  plot(1:i+1,gas3,'g');
+  plot(1:61,gas2,'b');
+  plot(1:61,gas3,'g');
   hold off
   xlabel( 'Gas presence' );
   ylabel( 'Time' );
   title( "Air quality" );
   legend('Arduino 1', 'Arduino 2', 'Arduino 3')
-  saveas (1, "../Final\ Report/Figures/GraphicGas.png");
+  saveas (5, "../Final\ Report/Figures/GraphicGas.png");
 
 end
