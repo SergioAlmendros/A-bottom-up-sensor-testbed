ef0ea62dae2c26c39e44f3972538997045d11f8b
Sergio <sergio.almendros.diaz@gmail.com>
2014-06-13 15:08:51 +0200

    testbed results report

--- Collect Data From Sensor Nodes/collect.m
+++ Collect Data From Sensor Nodes/collect.m
@@ -1,69 +1,72 @@
 function collect()
-
-  fid = fopen ("logData1", 'r');
+	
+	limit = 100;
+  fid = fopen ("logDataFerran", 'r');
   i = 0;
   while ( feof(fid) == 0 )
-    i = i+1;
+    if( i == limit )
+    	break;
+  	end
+  	i = i+1;
     p = fgetl(fid);
     #disp(p)
     r = regexp(p, ' ', 'split');
-    
-    if(i < 62)
-      temp1(i) = str2double(r(2));
-      hum1(i) = str2double(r(3));
-      noise1(i) = str2double(r(4));
-      light1(i) = str2double(r(5));
-      gas1(i) = str2double(r(6));
-      time1(i) = r(7);
-    end
-
-  end
-  fclose(fid);
-  
-  fid = fopen ("logData2", 'r');
-  i = 0;
-  while ( feof(fid) == 0 )
-    i = i+1;
-    p = fgetl(fid);
-    #disp(p)
-    r = regexp(p, ' ', 'split');
-    if(i < 62)
-      temp2(i) = str2double(r(2));
-      hum2(i) = str2double(r(3));
-      noise2(i) = str2double(r(4));
-      light2(i) = str2double(r(5));
-      gas2(i) = str2double(r(6));
-      time2(i) = r(7);
-    end
+    temp1(i) = str2double(r(2));
+    hum1(i) = str2double(r(3));
+    noise1(i) = str2double(r(4));
+    light1(i) = str2double(r(5));
+    gas1(i) = str2double(r(6));
+    time1(i) = r(7);
 
   end
   fclose(fid);
   
-  fid = fopen ("logData3", 'r');
+  fid = fopen ("logDataSergio", 'r');
   i = 0;
   while ( feof(fid) == 0 )
-    i = i+1;
+    if( i == limit )
+    	break;
+  	end
+  	i = i+1;
     p = fgetl(fid);
     #disp(p)
     r = regexp(p, ' ', 'split');
-    if(i < 62)
-      temp3(i) = str2double(r(2));
-      hum3(i) = str2double(r(3));
-      noise3(i) = str2double(r(4));
-      light3(i) = str2double(r(5));
-      gas3(i) = str2double(r(6));
-      time3(i) = r(7);
-    end
+    temp2(i) = str2double(r(2));
+    hum2(i) = str2double(r(3));
+    noise2(i) = str2double(r(4));
+    light2(i) = str2double(r(5));
+    gas2(i) = str2double(r(6));
+    time2(i) = r(7);
+
+  end
+  fclose(fid);
+  
+  fid = fopen ("logDataUPF", 'r');
+  i = 0;
+  while ( feof(fid) == 0 )
+    if( i == limit )
+    	break;
+  	end
+  	i = i+1;
+    p = fgetl(fid);
+    #disp(p)
+    r = regexp(p, ' ', 'split');
+    temp3(i) = str2double(r(2));
+    hum3(i) = str2double(r(3));
+    noise3(i) = str2double(r(4));
+    light3(i) = str2double(r(5));
+    gas3(i) = str2double(r(6));
+    time3(i) = r(7);
     
   end
   fclose(fid);
 
 
   figure 1
-  plot(1:61,temp1,'r');
+  plot(1:i,temp1,'r');
   hold on
-  plot(1:61,temp2,'b');
-  plot(1:61,temp3,'g');
+  plot(1:i,temp2,'b');
+  plot(1:i,temp3,'g');
   hold off
   ylabel( 'Temperature' );
   xlabel( 'Time' );
@@ -72,10 +75,10 @@ function collect()
   saveas (1, "../Final\ Report/Figures/GraphicTemperature.png");
   
   figure 2
-  plot(1:61,hum1,'r');
+  plot(1:i,hum1,'r');
   hold on
-  plot(1:61,hum2,'b');
-  plot(1:61,hum3,'g');
+  plot(1:i,hum2,'b');
+  plot(1:i,hum3,'g');
   hold off
   ylabel( 'Humidity' );
   xlabel( 'Time' );
@@ -84,10 +87,10 @@ function collect()
   saveas (2, "../Final\ Report/Figures/GraphicHumidity.png");
   
   figure 3
-  plot(1:61,noise1,'r');
+  plot(1:i,noise1,'r');
   hold on
-  plot(1:61,noise2,'b');
-  plot(1:61,noise3,'g');
+  plot(1:i,noise2,'b');
+  plot(1:i,noise3,'g');
   hold off
   ylabel( 'Noise' );
   xlabel( 'Time' );
@@ -96,10 +99,10 @@ function collect()
   saveas (3, "../Final\ Report/Figures/GraphicNoise.png");
   
   figure 4
-  plot(1:61,light1,'r');
+  plot(1:i,light1,'r');
   hold on
-  plot(1:61,light2,'b');
-  plot(1:61,light3,'g');
+  plot(1:i,light2,'b');
+  plot(1:i,light3,'g');
   hold off
   ylabel( 'Light' );
   xlabel( 'Time' );
@@ -108,10 +111,10 @@ function collect()
   saveas (4, "../Final\ Report/Figures/GraphicLight.png");
   
   figure 5
-  plot(1:61,gas1,'r');
+  plot(1:i,gas1,'r');
   hold on
-  plot(1:61,gas2,'b');
-  plot(1:61,gas3,'g');
+  plot(1:i,gas2,'b');
+  plot(1:i,gas3,'g');
   hold off
   ylabel( 'Gas presence' );
   xlabel( 'Time' );
